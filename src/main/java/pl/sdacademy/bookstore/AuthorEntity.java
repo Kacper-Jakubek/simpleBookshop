f9fe4a7b30cbbe132d9c56407ceffc75aa16da6a
AL-O <laskowska.anna92@gmail.com>
2020-12-17 17:54:17 +0100

    dodanie AuthorEntity

---
+++ src/main/java/pl/sdacademy/bookstore/AuthorEntity.java
@@ -0,0 +1,59 @@
+package pl.sdacademy.bookstore;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AuthorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer Id;
+    private String AuthorsFname;
+    private String AuthorLname;
+
+    public AuthorEntity() {
+    }
+
+    public AuthorEntity(Integer Id,String AuthorsFname, String AuthorsLname) {
+        this.Id = Id;
+        this.AuthorLname = AuthorsLname;
+        this.AuthorsFname = AuthorsFname;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public AuthorEntity(Integer id) {
+        Id = id;
+    }
+
+    public String getAuthorsFname() {
+        return AuthorsFname;
+    }
+
+    public void setAuthorsFname(String authorsFname) {
+        AuthorsFname = authorsFname;
+    }
+
+    public String getAuthorLname() {
+        return AuthorLname;
+    }
+
+    public void setAuthorLname(String authorLname) {
+        AuthorLname = authorLname;
+    }
+
+    public AuthorEntity(String authorsFname, String authorLname) {
+        AuthorsFname = authorsFname;
+        AuthorLname = authorLname;
+    }
+}
