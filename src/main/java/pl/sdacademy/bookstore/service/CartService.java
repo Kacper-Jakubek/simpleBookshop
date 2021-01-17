31a4a6b9a9d0e5520a622b4d61059099c1ce6de0
Anna Ksiazek <aksiazek@softak.eu>
2021-01-17 10:34:57 +0100

    dodano szablon schedulera, którego zadaniem będzie czyszczenie koszyka
    + częstotliwość wykonania konfigurowana w application.yml

---
+++ src/main/java/pl/sdacademy/bookstore/service/CartService.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.bookstore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService implements ICartService {
+    @Override
+    public void clean() {
+        // implementacja realizowana przez K
+    }
+}
