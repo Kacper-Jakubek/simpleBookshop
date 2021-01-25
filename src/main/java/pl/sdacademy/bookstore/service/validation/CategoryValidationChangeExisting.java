bbea4ef535aeed7e04fb22763cb4246a302b214d
irek.m <irek.m@kurs.pl>
2021-01-25 23:15:17 +0100

    Dodana metoda w CategoryValidation sprawdzająca czy można zmienić parent category

--- src/main/java/pl/sdacademy/bookstore/service/validation/CategoryValidationChangeExisting.java
+++ src/main/java/pl/sdacademy/bookstore/service/validation/CategoryValidationChangeExisting.java
@@ -9,16 +9,24 @@ package pl.sdacademy.bookstore.service.validation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.sdacademy.bookstore.model.Category;
+import pl.sdacademy.bookstore.service.CategoryService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryValidationChangeExisting {
 
+    private static final String ERROR_MSG_PARENT_CATEGORY_CANNOT_BE_CHANGE_TO_CHILDREN = "Parent category cannot be change to children";
+    private static final String ERROR_MSG_CATEGORY_CANNOT_BE_NULL = "Category cannot be null";
+
+    private final CategoryService categoryService;
     private final CategoryValidationAddNew categoryValidationAddNew;
 
     @Autowired
-    public CategoryValidationChangeExisting(CategoryValidationAddNew categoryValidationAddNew) {
+    public CategoryValidationChangeExisting(CategoryService categoryService, CategoryValidationAddNew categoryValidationAddNew) {
+        this.categoryService = categoryService;
         this.categoryValidationAddNew = categoryValidationAddNew;
     }
 
@@ -33,5 +41,33 @@ public class CategoryValidationChangeExisting {
      */
     public List<String> checkName(Category category){
         return categoryValidationAddNew.checkName(category);
+    }
+
+    /**
+     * It validates if parent category is properly changed.
+     *
+     * @param category DTO instance
+     * @return list of error messages
+     *
+     * Parent category cannot be changed to category children
+     */
+    public List<String> checkParentCategory(Category category){
+        List<String> errorMessages = new ArrayList<>();
+
+        if(category==null){
+            errorMessages.add(ERROR_MSG_CATEGORY_CANNOT_BE_NULL);
+        }else{
+            //Cannot change parent category to yourself children
+            List<Category> children = categoryService.findChildren(category.getId());
+            children.stream()
+                    .filter(child -> child.getId() == category.getParentCategory().getId())
+                    .collect(Collectors.toList());
+
+            if(!children.isEmpty()){
+                errorMessages.add(ERROR_MSG_PARENT_CATEGORY_CANNOT_BE_CHANGE_TO_CHILDREN);
+            }
+        }
+
+        return errorMessages;
     }
 }
